fa8344cb0454487daea0034e5c3ed9bb44fea570
Sebastien <sebastien@sebs-studio.com>
2014-08-25 19:52:10 +0200

    Made some major adjustments to the Gruntfile.js file. Requires testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,138 @@
 module.exports = function(grunt) {
 
-// Load multiple grunt tasks using globbing patterns
-require('load-grunt-tasks')(grunt);
+  // Load multiple grunt tasks using globbing patterns
+  require('load-grunt-tasks')(grunt);
 
-// Project configuration.
-grunt.initConfig({
-  pkg: grunt.file.readJSON('package.json'),
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Setting directory of the boilerplate to fetch files
+    plugin_dir: { 
+      main: 'wordpress-plugin-boilerplate',
+    },
+
+    // Setting the assets folders
+    dirs: {
+      css: '<%= plugin_dir.main %>/assets/css',
+      fonts: '<%= plugin_dir.main %>/assets/fonts',
+      images: '<%= plugin_dir.main %>/assets/images',
+      js: '<%= plugin_dir.main %>/assets/js',
+      lang: '<%= plugin_dir.main %>/languages',
+    },
+
+    // Javascript linting with jshint.
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= dirs.js %>/admin/*.js',
+        '!<%= dirs.js %>/admin/*.min.js',
+        '<%= dirs.js %>/frontend/*.js',
+        '!<%= dirs.js %>/frontend/*.min.js'
+      ]
+    },
+
+    // Compile all .less files. - Will enable once less files are ready and released.
+    /*less: {
+      compile: {
+        options: {
+          // These paths are searched for @imports
+          paths: ['<%= dirs.css %>/']
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= dirs.css %>/',
+          src: [
+            '*.less',
+            '!wordpress-plugin-boilerplate-base.less',
+            '!mixins.less'
+          ],
+          dest: '<%= dirs.css %>/',
+          ext: '.css'
+        }]
+      }
+    },*/
+
+    // Minify all .js files.
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        preserveComments: 'some'
+      },
+      admin: {
+        files: [{
+          expand: true,
+          cwd: '<%= dirs.js %>/admin/',
+          src: [
+            '*.js',
+            '!*.min.js',
+            '!Gruntfile.js',
+          ],
+          dest: '<%= dirs.js %>/admin/',
+          ext: '.min.js'
+        }]
+      },
+      frontend: {
+        files: [{
+          expand: true,
+          cwd: '<%= dirs.js %>/frontend/',
+          src: [
+            '*.js',
+            '!*.min.js'
+          ],
+          dest: '<%= dirs.js %>/frontend/',
+          ext: '.min.js'
+        }]
+      },
+    },
+
+    // Minify all .css files.
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: '<%= dirs.css %>/',
+        src: ['*.css'],
+        dest: '<%= dirs.css %>/',
+        ext: '.css'
+      }
+    },
+
+    // Watch changes in the assets
+    watch: {
+      // Will enable less in the future.
+      /*less: {
+        files: ['<%= dirs.css %>/*.less'],
+        tasks: ['less', 'cssmin']
+      },*/
+      js: {
+        files: [
+          '<%= dirs.js %>/admin/*js',
+          '<%= dirs.js %>/frontend/*js',
+          '!<%= dirs.js %>/admin/*.min.js',
+          '!<%= dirs.js %>/frontend/*.min.js'
+        ],
+        tasks: ['uglify']
+      },
+      css: {
+        files: [
+          '<%= dirs.js %>/admin/*js',
+          '<%= dirs.js %>/*js',
+          '!<%= dirs.js %>/admin/*.min.js',
+          '!<%= dirs.js %>/*.min.js'
+        ],
+        tasks: ['cssmin']
+      }
+    },
 
     makepot: {
       target: {
         options: {
-          domainPath: 'wordpress-plugin-boilerplate/languages/',    // Where to save the POT file.
+          domainPath: '<%= plugin_dir.main %>/languages/',    // Where to save the POT file.
           exclude: ['build/.*'],
-          mainFile: 'wordpress-plugin-boilerplate/wordpress-plugin-boilerplate.php',    // Main project file.
+          mainFile: '<%= plugin_dir.main %>/wordpress-plugin-boilerplate.php',    // Main project file.
           potComments: 'WordPress Plugin Boilerplate Copyright (c) {{year}}',      // The copyright at the beginning of the POT file.
           potFilename: 'wordpress-plugin-boilerplate.pot',    // Name of the POT file.
           type: 'wp-plugin',    // Type of project.
@@ -35,6 +155,34 @@ grunt.initConfig({
       }
     },
 
+    checktextdomain: {
+      options:{
+        text_domain: 'wordpress-plugin-boilerplate',
+        keywords: [
+          '__:1,2d',
+          '_e:1,2d',
+          '_x:1,2c,3d',
+          'esc_html__:1,2d',
+          'esc_html_e:1,2d',
+          'esc_html_x:1,2c,3d',
+          'esc_attr__:1,2d',
+          'esc_attr_e:1,2d',
+          'esc_attr_x:1,2c,3d',
+          '_ex:1,2c,3d',
+          '_n:1,2,4d',
+          '_nx:1,2,4c,5d',
+          '_n_noop:1,2,3d',
+          '_nx_noop:1,2,3c,4d'
+        ]
+      },
+      files: {
+        src:  [
+          '**/*.php', // Include all files
+        ],
+        expand: true
+      }
+    },
+
     exec: {
       npmUpdate: {
         command: 'npm update'
@@ -47,32 +195,23 @@ grunt.initConfig({
       },
     },
 
-         dirs: {
-    lang: 'wordpress-plugin-boilerplate/languages',
-    },
-
     potomo: {
       dist: {
         options: {
-         poDel: false // Set to true if you want to erase the .po
+          poDel: false // Set to true if you want to erase the .po
         },
         files: [{
-         expand: true,
-         cwd: '<%= dirs.lang %>',
+          expand: true,
+          cwd: '<%= dirs.lang %>',
           src: ['*.po'],
           dest: '<%= dirs.lang %>',
-         ext: '.mo',
+          ext: '.mo',
           nonull: true
-      }]
-    }
-  },
-
-    // Clean up build directory
-    clean: {
-      main: ['build/<%= pkg.name %>']
+        }]
+      }
     },
 
-    // Copy the theme into the build directory
+    // Copy the plugin into the build directory
     copy: {
       main: {
         src:  [
@@ -100,7 +239,12 @@ grunt.initConfig({
       }
     },
 
-    //Compress build directory into <name>.zip and <name>-<version>.zip
+    // Clean up build directory
+    clean: {
+      main: ['build/<%= pkg.name %>']
+    },
+
+    // Compress build directory into <name>.zip and <name>-<version>.zip
     compress: {
       main: {
         options: {
@@ -114,18 +258,30 @@ grunt.initConfig({
       }
     },
 
-});
+  });
 
-// Default task. - grunt makepot
-grunt.registerTask( 'default', 'makepot' );
+  // Load NPM tasks to be used here
+  grunt.loadNpmTasks( 'load-grunt-tasks' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+  //grunt.loadNpmTasks( 'grunt-contrib-less' );
+  grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+  grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+  grunt.loadNpmTasks( 'grunt-contrib-copy' );
+  grunt.loadNpmTasks( 'grunt-contrib-clean' );
+  grunt.loadNpmTasks( 'grunt-wp-i18n' );
+  grunt.loadNpmTasks( 'grunt-checktextdomain' );
 
-// Makepot and push it on Transifex task(s).
-grunt.registerTask( 'makandpush', [ 'makepot', 'exec:txpush_s' ] );
+  // Register Tasks
+  grunt.registerTask( 'default', ['makepot', 'jshint', 'cssmin', 'uglify']);
 
-// Pull from Transifex and create .mo task(s).
-grunt.registerTask( 'tx', [ 'exec:txpull', 'potomo' ] );
+  // Makepot and push it on Transifex task(s).
+  grunt.registerTask( 'makandpush', [ 'makepot', 'exec:txpush_s' ] );
 
-// Build task(s).
+  // Pull from Transifex and create .mo task(s).
+  grunt.registerTask( 'tx', [ 'exec:txpull', 'potomo' ] );
+
+  // Build task(s).
   grunt.registerTask( 'build', [ 'clean', 'copy', 'compress' ] );
 
 };
